14f9c27ce97921d2984ba63a69baae68fce3cd1f
curtjenk <curtjenk@comcast.net>
2016-04-25 13:39:26 -0400

    added compass

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var mongo = require('mongodb');
-var mongoClient = mongo.MongoClient;
-var mongoUrl = 'mongodb://localhost:27017/electricOrNot';
-var db;
-
-mongoClient.connect(mongoUrl, function(error, database) {
-   if (error) {
-    console.log(error);
-   }
-   // console.log('connected')
-    db = database;
-});
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.collection('cars').find({}).toArray(function(error, carResult) {
-   // console.log(carResult);
-    // for (var i=0; i<carResult.length; i++) {
-    //   console.log(carResult[i].imageSrc);
-    // }
-    var ranNdx = Math.floor(Math.random() * carResult.length);
-    console.log(carResult);
-    res.render('index', { carImage: carResult[ranNdx].imageSrc });
-  });
-  //console.log(cars);
-  // res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
+});
+
+/* Set up the post electric page. */
+router.post('/electric', function(req, res, next){
+  res.send(req.body);
 });
 
 module.exports = router;
