d9a9319a03306a49d593f7794edffffe6380928a
curtjenk <curtjenk@comcast.net>
2016-04-26 17:58:43 -0400

    Implemented Reset feature

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var mongoClient = require('mongodb').MongoClient;
 //setup to accomdate running on Heroku
-var mongoUrl = process.env.MONGOLAB_URI ||
+var mongoUrl = process.env.MONGODB_URI ||
     process.env.MONGOHQ_URL ||
     'mongodb://localhost:27017/electricOrNot';
 
@@ -12,15 +12,10 @@ var photosToShow = [];
 
 //create a db connection and assign it to a var
 mongoClient.connect(mongoUrl, function(error, database) {
-        db = database;
-        database.collection('cars').find().toArray(function(error, result) {
-            allPhotos = result;
-            db = database;
-            // console.log(allPhotos);
-        })
+    db = database;
+})
 
-    })
-    /* GET home page. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
     var currIP = req.ip;
     db.collection('users').find({ ip: currIP }).toArray(function(error, userResult) {
@@ -28,11 +23,11 @@ router.get('/', function(req, res, next) {
         userResult.forEach(function(val) {
                 photosVoted.push(val.image);
             })
-        //$nin is a mongodb key word for "not in"
-        //get all the cars that the user has not voted on.
-        //if voted on every image, send him to the standings page.
+            //$nin is a mongodb key word for "not in"
+            //get all the cars that the user has not voted on.
+            //if voted on every image, send him to the standings page.
         db.collection('cars').find({ imageSrc: { $nin: photosVoted } }).toArray(function(error, result) {
-            console.log(result)
+            //console.log(result)
             if (result.length > 0) {
                 var rndx = Math.floor(Math.random() * result.length);
                 res.render('index', { carImage: result[rndx].imageSrc });
@@ -55,7 +50,43 @@ router.get('/standings', function(req, res, next) {
 
 })
 
+router.get('/reset', function(req, res, next) {
+    console.log("resetting/reverting all my votes");
+    var currIP = req.ip;
+    db.collection('users').find({ ip: currIP }).toArray(function(error, userResult) {
+        for (var i = 0; i < userResult.length; i++) {
+            var photo = userResult[i].image;
+            var vote = userResult[i].vote;
+            console.log(photo + ' ' + vote);
 
+            db.collection('cars').find({ imageSrc: photo }).toArray(function(error, result) {
+                var newTotal = 0;
+                console.log(result[0].imageSrc + ' Old totalVotes = ' + result[0].totalVotes);
+                if (vote == 'electric') {
+                    newTotal = result[0].totalVotes - 1;
+                } else if (vote == 'poser') {
+                    newTotal = result[0].totalVotes + 1;
+                }
+                console.log(result[0].imageSrc + ' NEW totalVotes = ' + newTotal);
+                
+                db.collection('cars').updateOne({ "imageSrc": result[0].imageSrc }, {
+                        $set: { "totalVotes": newTotal }
+                    },
+                    function(error, results) {
+                        console.log("===== delete user record for car ");
+                        console.log(results);
+                     
+                        db.collection('users').deleteOne({ ip: currIP, image: result[0].imageSrc }, function(error, delResult) {
+
+                        });
+                    }
+                );
+
+            })
+        }
+    });
+    res.redirect('/'); //6. send back to home page.
+})
 
 /* Set up the post electric page. */
 router.post('/electric', function(req, res, next) {
@@ -99,7 +130,7 @@ router.post('/poser', function(req, res, next) {
     */
     db.collection('users').insertOne({
         ip: req.ip,
-        vote: 'electric',
+        vote: 'poser',
         image: req.body.photo
     })
     db.collection('cars').find({ imageSrc: req.body.photo }).toArray(function(error, result) {
